6c254d98ab9d2badd0cd2f0358dc7a28f4974d82
Martin Markov <mmarkov93@gmail.com>
2011-11-12 23:23:36 +0200

    Initial Commit

---
+++ NorrisJokes/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NorrisJokes
+//
+//  Created by Martin Markov on 11/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
